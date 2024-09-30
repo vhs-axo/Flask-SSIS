25e2aa4367049e952942ab12cef4c90c6bb2e52e
vhs-axo <mkpalarpalar@gmail.com>
2024-09-30 22:16:22 +0800

    implemented connect and connection properties

--- src/flask_mysql_connector/__init__.py
+++ src/flask_mysql_connector/__init__.py
@@ -26,7 +26,40 @@ class FlaskMySQLConnector:
 
         # Register teardown function to close DB connections after request
         app.teardown_appcontext(self.teardown)
+
+    @property
+    def connect(self) -> PooledMySQLConnection | MySQLConnectionAbstract:
+        """
+        Establishes a new MySQL database connection using the Flask app's configuration.
+
+        Returns:
+            mysql.connector.connection.MySQLConnection: A MySQL connection object.
+        """
+        try:
+            connection = mysql.connector.connect(
+                host=current_app.config["MYSQL_HOST"],
+                port=current_app.config["MYSQL_PORT"],
+                user=current_app.config["MYSQL_USER"],
+                password=current_app.config["MYSQL_PASSWORD"],
+                database=current_app.config["MYSQL_DB"]
+            )
+            return connection
+        except Error as e:
+            current_app.logger.error(f"Error connecting to MySQL: {e}")
+            raise e
     
+    @property
+    def connection(self) -> PooledMySQLConnection | MySQLConnectionAbstract:
+        """
+        Retrieves or creates a MySQL database connection for the current Flask request context.
+
+        Returns:
+            mysql.connector.connection.MySQLConnection: The MySQL connection object.
+        """
+        if "db_connection" not in ctx:
+            ctx.db_connection = self.connect
+        return ctx.db_connection
+
     def teardown(self, exception: Optional[BaseException] = None) -> None:
         connection: Optional[PooledMySQLConnection | MySQLConnectionAbstract] = ctx.pop("db_connection", None)
         if connection is not None:
