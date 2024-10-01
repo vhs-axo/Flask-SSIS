eac6baf7b9ef8f6757c5b20be575618faa6898f3
vhs-axo <mkpalarpalar@gmail.com>
2024-10-01 10:31:46 +0800

    facotry method for creating the flask app

---
+++ src/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from config import MYSQL_DB, MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD
+
+from src.flask_mysql_connector import FlaskMySQLConnector
+
+mysql = FlaskMySQLConnector()
+
+def create_app() -> Flask:
+    """
+    Factory function to create a Flask application.
+    This function initializes the app, configures it, and sets up MySQL and blueprints.
+    """
+    # Create a Flask application instance
+    app = Flask(__name__)
+
+    # Load configuration from a config file or environment variables
+    app.config.from_mapping(
+        MYSQL_USER=MYSQL_USER,
+        MYSQL_PASSWORD=MYSQL_PASSWORD,
+        MYSQL_DB=MYSQL_DB,
+        MYSQL_HOST=MYSQL_HOST
+    )
+
+    # Initialize the MySQL connection
+    mysql.init_app(app)
+
+    # Return the Flask application instance
+    return app
