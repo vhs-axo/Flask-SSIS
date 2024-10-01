0dcb0df4c65d86ac04ba0eed427483040685d042
vhs-axo <mkpalarpalar@gmail.com>
2024-10-01 16:18:16 +0800

    implemente __init__ for forms module

---
+++ src/forms/__init__.py
@@ -0,0 +1,5 @@
+from src.forms.college_form import CollegeForm
+from src.forms.program_form import ProgramForm
+from src.forms.student_form import StudentForm
+
+__all__ = ["CollegeForm", "ProgramForm", "StudentForm"]
