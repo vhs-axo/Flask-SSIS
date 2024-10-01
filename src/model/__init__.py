9e0cc895ab225b4312ccc0b95fbe622023cd01ea
vhs-axo <mkpalarpalar@gmail.com>
2024-10-01 10:33:32 +0800

    implemted base static methods for getting, adding, editing, and deleting entities

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,3 +1,16 @@
+from __future__ import annotations
+
+from collections.abc import Iterator
+from typing import Optional, TypeVar
+
+from mysql.connector.cursor import MySQLCursorDict
+from mysql.connector import Error
+
+from src.entities import College, Program, Student
+from src import mysql
+
+T = TypeVar("T", bound=College | Program | Student)
+
 class SSIS:
     INSERT_COLLEGE_QUERY = "INSERT INTO colleges (code, name) VALUES (%s, %s)"
     INSERT_PROGRAM_QUERY = "INSERT INTO programs (code, name, college) VALUES (%s, %s, %s)"
@@ -18,3 +31,65 @@ class SSIS:
     UPDATE_COLLEGE_QUERY = "UPDATE colleges SET name = %s WHERE code = %s"
     UPDATE_PROGRAM_QUERY = "UPDATE programs SET name = %s, college = %s WHERE code = %s"
     UPDATE_STUDENT_QUERY = "UPDATE students SET firstname = %s, lastname = %s, year = %s, gender = %s, program = %s WHERE id = %s"
+
+    @staticmethod
+    def __add_entity(query: str, params: tuple[str | int, ...]) -> None:
+        try:
+            cursor: MySQLCursorDict
+            with mysql.connection.cursor() as cursor: # type: ignore
+                cursor.execute(query, params)
+                mysql.connection.commit()
+        
+        except Error as e:
+            mysql.connection.rollback()
+            raise
+    
+    @staticmethod
+    def __get_entity(query: str, primary_key: str, return_type: type[T]) -> Optional[T]:
+        try:
+            cursor: MySQLCursorDict
+            with mysql.connection.cursor(dictionary=True) as cursor: # type: ignore
+                cursor.execute(query, (primary_key,))
+
+                if (row := cursor.fetchone()) is not None:
+                    return return_type.from_db_row(row)
+                return None
+        
+        except Error as e:
+            raise
+    
+    @staticmethod
+    def __get_entites(query: str, return_type: type[T]) -> Iterator[T]:
+        try:
+            cursor: MySQLCursorDict
+            with mysql.connection.cursor(dictionary=True) as cursor: # type: ignore
+                cursor.execute(query)
+
+                return (return_type.from_db_row(row) for row in cursor.fetchall() if row is not None)
+        
+        except Error as e:
+            raise
+    
+    @staticmethod
+    def __delete_entity(query: str, primary_key: str) -> None:
+        try:
+            cursor: MySQLCursorDict
+            with mysql.connection.cursor(dictionary=True) as cursor: # type: ignore
+                cursor.execute(query, (primary_key,))
+                mysql.connection.commit()
+        
+        except Error as e:
+            mysql.connection.rollback()
+            raise
+    
+    @staticmethod
+    def __edit_entity(query: str, params: tuple[str | int, ...]) -> None:
+        try:
+            cursor: MySQLCursorDict
+            with mysql.connection.cursor() as cursor: # type: ignore
+                cursor.execute(query, params)
+                mysql.connection.commit()
+        
+        except Error as e:
+            mysql.connection.rollback()
+            raise
