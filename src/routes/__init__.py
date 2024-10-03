794e8cf09fef359708d1f5504cc87ad26251179d
vhs-axo <mkpalarpalar@gmail.com>
2024-10-03 11:20:29 +0800

    route __init__

---
+++ src/routes/__init__.py
@@ -0,0 +1,5 @@
+from src.routes.college_controller import colleges_bp
+from src.routes.program_controller import programs_bp
+from src.routes.student_controller import students_bp
+
+__all__ = ["colleges_bp", "programs_bp", "students_bp"]
